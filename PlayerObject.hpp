3122d2fde618cb99ef1d3eb9b2ac72a2dd20b5dd
fanfreak2247 <keandrewinston@gmail.com>
2020-04-14 13:28:47 -0400

    Update PlayerObject.hpp

--- PlayerObject.hpp
+++ PlayerObject.hpp
@@ -1,11 +1,24 @@
 #include <SFML/Graphics.hpp>
+template <typename T>
+std::string to_string(const sf::Vector2<T>& vec) {
+  std::string str = "(";
+  str += std::to_string(vec.x);
+  str += ",";
+  str += std::to_string(vec.y);
+  str += ')';
 
+  return str;
+}
+enum horizontalMovement{Left, None, Right};
 class PlayerObject{
 private:
   sf::RectangleShape rect{sf::Vector2f(10,10)};
   sf::Vector2f velocity{0,0};
-  sf::Vector2f acceleration{0,0.00001};
-  bool inAir = true;
+  sf::Vector2f acceleration{0,0.05};
+  sf::Vector2f horizontalAcceleration{0.5,0};
+  horizontalMovement direction = horizontalMovement::None;
+  bool jumping = false;
+  bool collide = false;
 public:
   PlayerObject(unsigned int x, unsigned int y){
     rect.setFillColor(sf::Color::Green);
@@ -13,21 +26,100 @@ public:
   }
 
   void update(){
-    if(inAir){
-        rect.move(velocity);
-        velocity += acceleration;
+    if(collide){
+      acceleration.y = 0;
+	  velocity.y = 0;
+    }
+    else{
+      acceleration={0,0.5};
+    }
+	if(jumping){
+      acceleration={0,-0.9};
+      collide = false;
+    }
+    if(velocity.y <= -6.2){
+      jumping=false;
+    }
+	//horizontal movement
+	if(direction==horizontalMovement::Right){
+		velocity+=horizontalAcceleration;
+		if(velocity.x>2)velocity.x=2;
+	}
+	else if(direction==horizontalMovement::Left){
+		velocity-=horizontalAcceleration;
+		if(velocity.x<-2)velocity.x=-2;
+	}
+	else{
+		velocity.x = 0;
+	}
+	velocity += acceleration;
+    //change acceleration back to normal
+
+    rect.move(velocity);
+  }
+  void changeJumpStatus(bool jump){
+    if(collide) {
+      jumping=jump;
+      collide=false;
     }
   }
 
-  void changeAirStatus(bool status){
-    inAir=status;
+  void setCollisionStatus(bool colliding){
+    collide=colliding;
   }
-
+  bool getJumpStatus(){
+    return jumping;
+  }
+  bool getCollisionStatus(){
+    return collide;
+  }
+  
+  void setMovingRight(bool movingRight){
+	  if(movingRight){
+		  direction=horizontalMovement::Right;
+	  }
+	  else{
+		  direction=horizontalMovement::None;
+	  }
+  }
+  void setMovingLeft(bool movingLeft){
+	  if(movingLeft){
+		  direction=horizontalMovement::Left;
+	  }
+	  else{
+		  direction=horizontalMovement::None;
+	  }
+  }
+	
+  std::string getMovementDirection(){
+	switch(this->direction){
+		case horizontalMovement::Right:
+			return "Right";
+		case horizontalMovement::Left:
+			return "Left";
+		case horizontalMovement::None:
+			return "Stationary";
+		default:
+			return "Right";
+	}
+  }
+	  
   sf::RectangleShape getPlayerSprite(){
     return rect;
   }
-
   void move(int x, int y){
     rect.move(x,y);
+  }
+  sf::Vector2f getVelocity(){
+    return velocity;
+  }
+  sf::Vector2f getAcceleration(){
+    return acceleration;
+  }
+  sf::Vector2f getPosition(){
+    return rect.getPosition();
+  }
+  sf::FloatRect getGlobalBounds(){
+    return rect.getGlobalBounds();
   }
 };
